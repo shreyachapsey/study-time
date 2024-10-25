1ca2af5d08f31d777068fc41eebce904bb2f1ed0
Shreya Chapsey <chaps005@umn.edu>
2024-10-24 21:11:00 -0500

    changes

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
 chrome.alarms.create({
-    periodInMinutes: 1/60,
-})    //runs all the time
+    periodInMinutes: 1/60,  // runs every second
+});
 
-chrome.alarms.onAlarm.addListener((alarm)=>{
-    chrome.storage.local.get(["timer"],(res)=>{
-        const time = res.timer ?? 0
-    chrome.storage.local.set({
-        timer:time+1
-    }) 
-    chrome.action.setBadgeText({
-        text:`${time+1}`
-    })   
+chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.storage.local.get(["timer"], (res) => {
+        const time = res.timer ?? 0;
+        
+        chrome.storage.local.set({
+            timer: time + 1
+        });
+        
+        chrome.action.setBadgeText({
+            text: `${time + 1}`
+        });
+        chrome.storage.sync.get(["notificationTime"],(res)=>{
+            const notificationTime = res.notificationTime??1000
+        if (time%notificationTime==0){
+        this.registration.showNotification("Timer ext", {
+            body: `${notificationTime} seconds have passed`,
+            icon: "icon.png"
+        })
+    }
+    })
 })
 })
--- options.js
+++ options.js
@@ -1,15 +1,16 @@
 const nameInput = document.getElementById("name-input")
 const saveBtn = document.getElementById("save-btn")
-
+const timeInput = document.getElementById("time-input")
 saveBtn.addEventListener("click",()=>{
     const name = nameInput.value
+    const notificationTIme = timeInput.value
     chrome.storage.sync.set({
         name,
-    },()=>{
-            console.log(`Name is set to ${name}`)
-        })
+        notificationTIme,
+    })
     })
 
 chrome.storage.sync.get(["name"],(res)=>{
-    nameInput.vale = res.name??"???"
+    nameInput.value = res.name??"???"
+    timeInput.value = res.notificationTime?? 10000
 })
